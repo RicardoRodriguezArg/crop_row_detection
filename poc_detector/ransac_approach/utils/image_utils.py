80d507442ea533af89e44b5a76eef456baf99efd
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-02-22 12:28:48 -0500

    added latest manual segmentation

--- poc_detector/ransac_approach/utils/image_utils.py
+++ poc_detector/ransac_approach/utils/image_utils.py
@@ -4,9 +4,12 @@ import matplotlib as plt
 
 class ImageLoader():
     def __init__(self, filename=None):
-        self.__bin_image = cv2.imread(filename, cv2.IMREAD_COLOR)
+        self.__image = cv2.imread(filename, cv2.IMREAD_COLOR)
+        self.__create_images_channel()
 
     @property
     def image(self):
-        return self.__bin_image
+        return self.__image
 
+    def __create_images_channel(self):
+        self.__blue_channel, self.__green_channel, self.__red_channel = cv2.split(self.__image)
