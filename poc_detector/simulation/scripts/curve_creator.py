9040c08a8b6b32bdbdb7c8b3d562631c9e61d8b3
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-10-13 13:59:41 +0200

    removed simulation folder from poc folder

--- poc_detector/simulation/scripts/curve_creator.py
+++
@@ -1,9 +0,0 @@
-import bpy
-
-class CurveCreator():
-	def __init__(self, x_points, y_points, plane):
-		self.x_list = x_points
-		self.y_list = y_points
-		self.z_list = None
-		self.plane = plane
-
--- poc_detector/simulation/scripts/launch_blender.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo Launching Blender
-#calling initial scripts
-python clothoides.py
-~/Desktop/blender-2.79b-linux-glibc219-x86_64/blender --python create_row_fields.py
