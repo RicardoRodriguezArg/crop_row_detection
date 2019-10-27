c124973217a1ac90b64bb7e26671dd39acee39a8
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-10-27 22:45:47 +0100

    add missing files

---
+++ map_compiler/website/principal/map_compiler_site.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, send_from_directory
+
+from utils import ServerConfig
+from utils import Utils
+
+
+app = Flask(__name__)
+server_config = ServerConfig('./config.ini')
+utils = Utils(server_config)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    section = server_config['UPLOAD_DIRECTORIES']
+    directory_to_save = section['RAW_AERIALS_IMAGES']
+    for upload in request.files.getlist("file"):
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        if not (utils.is_file_allowed(filename)):
+            print "Not allowed File: {}".format(filename)
+        else:
+            print("Save it to:", directory_to_save)
+            upload.save(directory_to_save)
+
+    return render_template("image_upload_complete.html")
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
---
+++ map_compiler/website/principal/utils.py
@@ -0,0 +1,41 @@
+import ConfigParser
+
+
+class ServerConfig:
+    def __init__(self, ini_file):
+        config = ConfigParser()
+        config.read(ini_file)
+        self.__config_dict = self.__create_config_dict(config)
+
+    def __create_config_dict(self, config):
+        result = dict()
+        for section in config.sections():
+            result[section] = self.__options_section_map(section, config)
+        return result
+
+    def __options_section_map(self, section, config):
+        dict1 = {}
+        options = config.options(section)
+        for option in options:
+            try:
+                dict1[option] = config.get(section, option)
+                if dict1[option] == -1:
+                    print("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                dict1[option] = None
+        return dict1
+
+    @property
+    def configuration_dict(self):
+        return self._config_dict
+
+
+class Utils:
+    def __init__(self, config):
+        section = config['ALLOWED_EXTENSIONS']
+        self.allowed_extensions = section.split(',').strip().lower()
+
+    def is_file_allowed(self, file):
+        extension = file.split('.')[1]
+        return extension in self.allowed_extensions
