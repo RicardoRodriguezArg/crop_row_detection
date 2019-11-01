61d2fea8ae2dbef00e290fef1a71f614e5a255cf
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-01 23:37:32 +0100

    add setup file 2

--- map_compiler/website/setup.py
+++ map_compiler/website/setup.py
@@ -4,6 +4,7 @@ setup(
     name='MapCompilerWebSite',
     version='0.1dev',
     packages=['main_page',],
+    package_data={'main_page': ['data/*.ini']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
