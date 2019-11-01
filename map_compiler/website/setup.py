68fae2cfd25619352b2c779283311229a28d8894
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-01 23:29:17 +0100

    add setup file

--- map_compiler/website/setup.py
+++ map_compiler/website/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='TowelStuff',
+    name='MapCompilerWebSite',
     version='0.1dev',
-    packages=['towelstuff',],
+    packages=['main_page',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
