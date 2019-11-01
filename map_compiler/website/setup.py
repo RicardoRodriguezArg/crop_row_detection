409f312af745b9f95e65b427b859630c467b5d19
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-02 00:21:51 +0100

    add manifest files

--- map_compiler/website/setup.py
+++ map_compiler/website/setup.py
@@ -6,5 +6,8 @@ setup(
     packages=['main_page',],
     package_data={'main_page': ['data/*.ini']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=['Flask'],
+    include_package_data=True,
+    zip_safe=False,
     long_description=open('README.txt').read(),
 )
