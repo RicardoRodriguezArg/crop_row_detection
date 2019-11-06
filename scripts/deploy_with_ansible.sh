55000a20d072f1255e596fac33cb1f9fee641c2f
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-06 22:46:24 +0100

    fix sintax 4

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "operador" | sudo -S -u ci_operator ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K
+echo "operator" | sudo -S -u ci_operator ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K
