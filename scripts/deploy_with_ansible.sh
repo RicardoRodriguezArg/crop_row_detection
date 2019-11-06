ea5225b08b533d8d6028ea8fcf6d733ba14eead6
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-06 23:35:18 +0100

    fix script

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -3,4 +3,4 @@ echo $USER
 sudo -u ci_operator 
 echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
-echo "operator" | sudo -i ansible-playbook --user -i ci_operator /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /home/ci_operator/.ssh/id_rsa
+echo "operator" | sudo -S ansible-playbook --user ci_operator -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /home/ci_operator/.ssh/id_rsa
