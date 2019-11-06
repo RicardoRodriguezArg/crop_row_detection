ed5ead54ca86738e7e43718f9ff448543ad2ff13
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-06 23:31:23 +0100

    setup initial config directory

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo $USER
-sudo -u ci_operator ansible-playbook --user -i ci_operator /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /home/ci_operator/.ssh/id_rsa
+sudo -u ci_operator 
+echo $USER
+export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
+echo "operator" | sudo -i ansible-playbook --user -i ci_operator /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /home/ci_operator/.ssh/id_rsa
