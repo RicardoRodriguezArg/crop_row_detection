cfc6654008ecdb6b2a70a59f86968933e11c4c2c
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-06 23:55:06 +0100

    updating script 3

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo $USER
+su - ci_operator
 echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
-su - ci_operator
 echo "operator" | sudo -S ansible-playbook --user ci_operator -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /home/ci_operator/.ssh/id_rsa -vvv
