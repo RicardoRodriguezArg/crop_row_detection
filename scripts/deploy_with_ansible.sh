65c7830ec20d16d831a9506ad404efaa4f12da28
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-09 14:34:39 +0100

    adding mc step for deploy and launch site 4

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
-#ansible all -m ping
 ansible-playbook ../playbook/playbook.yml -K --extra-vars "ansible_become_pass=operator"
+ansible-playbook ../map_compiler/playbooks/website/playbook.yml -K --extra-vars "ansible_become_pass=operator"
