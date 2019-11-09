1603d5e95b290591406d7636ae9b535a77bfd023
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-09 01:14:56 +0100

    updating script 3

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
-echo "operator" | sudo -S ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /var/lib/jenkins/.ssh/id_rsa.pub
+echo "operator" | sudo -S ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /var/lib/jenkins/.ssh/id_rsa
