9f13b03efd8f9b6d125511ace8758073d9ac4ea7
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-09 01:42:35 +0100

    updating script 3

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
-echo "operator" | sudo -S ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /var/lib/jenkins/.ssh/id_rsa
+ansible all -m ping
+#echo "operator" | sudo -S ansible-playbook -i /etc/ansible/hosts ../playbook/playbook.yml -K --key-file /var/lib/jenkins/.ssh/id_rsa --disableHostKeyChecking true
