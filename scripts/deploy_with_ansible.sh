ce20d623e57e09a5e9fed3dd02d5801b12f86889
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-09 16:35:43 +0100

    fix yaml sintax 2

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -2,4 +2,5 @@
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
 ansible-playbook ../playbook/playbook.yml -K --extra-vars "ansible_become_pass=operator"
 pwd
+echo "deploying web site on target machine"
 ansible-playbook ../map_compiler/playbooks/website/playbook.yml -K --extra-vars "ansible_become_pass=operator"
