120d30dce0afdc69c9b15cc0450cf0756cbd9f55
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-09 02:15:38 +0100

    updating script 5

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -2,4 +2,4 @@
 echo $USER
 export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
 #ansible all -m ping
-ansible-playbook ../playbook/playbook.yml -K
+ansible-playbook ../playbook/playbook.yml -K --extra-vars "ansible_become_pass=operator"
