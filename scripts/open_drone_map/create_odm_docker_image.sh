97336e7bb267183480e92ab608c2bf7b262440cc
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-11 18:23:36 +0100

    fix bash syntax

--- scripts/open_drone_map/create_odm_docker_image.sh
+++ scripts/open_drone_map/create_odm_docker_image.sh
@@ -3,12 +3,11 @@ DIR="input_data"
 if [ -d "$DIR" ]; then
   # Take action if $DIR exists. #
   echo "Directory exist ${DIR}!"
-
-fi
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "Error: ${DIR} not found. Creating input data folder for images."
   mkdir ${DIR}
 fi
-  echo "Building ODM docker image"
-  docker build -t opendronemap-image .
+
+echo "Building ODM docker image"
+docker build -t opendronemap-image .
