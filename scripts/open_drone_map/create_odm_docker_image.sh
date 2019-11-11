9869ff4ee6f7f57198a4f783e23173c386d417c6
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-11 19:11:16 +0100

    update docker odm script 2

--- scripts/open_drone_map/create_odm_docker_image.sh
+++ scripts/open_drone_map/create_odm_docker_image.sh
@@ -14,5 +14,6 @@ docker run -it --rm \
     -v $(pwd)/images:/code/images \
     -v $(pwd)/odm_orthophoto:/code/odm_orthophoto \
     -v $(pwd)/odm_texturing:/code/odm_texturing \
+    -v $(pwd)/odm_georeferencing:/code/odm_georeferencing \
     odm_image
 
