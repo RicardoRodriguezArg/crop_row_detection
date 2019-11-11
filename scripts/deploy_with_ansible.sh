f069659fd30f16c4373606e2ccccaa56a0e6a672
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-11-11 18:17:36 +0100

    fix path

--- scripts/deploy_with_ansible.sh
+++ scripts/deploy_with_ansible.sh
@@ -3,4 +3,4 @@ export ANSIBLE_CONFIG=/etc/ansible/ansible.cfg
 ansible-playbook ../playbook/playbook.yml -K --extra-vars "ansible_become_pass=operator"
 pwd
 echo "deploying web site on target machine"
-ansible-playbook ../map_compiler/playbooks/website/playbook.yml -K --extra-vars "ansible_become_pass=operator"
+ansible-playbook ../map_compiler/playbooks/odm/odm.yml -K --extra-vars "ansible_become_pass=operator"
