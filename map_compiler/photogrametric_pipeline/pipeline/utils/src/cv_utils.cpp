e3cab8b09ab2afb935cf46c1d7b3083393d1af15
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2019-12-30 23:26:19 +0100

    fix ut 9

--- map_compiler/photogrametric_pipeline/pipeline/utils/src/cv_utils.cpp
+++ map_compiler/photogrametric_pipeline/pipeline/utils/src/cv_utils.cpp
@@ -3,22 +3,16 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 
-cv::Mat NSFeatureExtraction::Utils::convertToGreyScale(const cv::Mat &image) {
-    cv::Mat result_image;
-    cv::cvtColor(image, cv::COLOR_BGR2GRAY);
-    return result_image;
+cv::Mat NSFeatureExtraction::Utils::convertToGreyScale(const cv::Mat &color_image) {
+    cv::Mat grey_image;
+    cv::cvtColor(color_image, grey_image, cv::COLOR_BGR2GRAY);
+    return grey_image;
 }
 
 cv::Mat NSFeatureExtraction::Utils::loadImageFromFileName(const std::string &image_filename) {
     cv::Mat result_image;
-
     if (!image_filename.empty()) {
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            printf("Current working dir: %s\n", cwd);
-        }
         result_image = cv::imread(image_filename, cv::IMREAD_COLOR);
-        std::cout << "result_image Empty: " << std::boolalpha << result_image.empty() << std::endl;
     }
     return result_image;
 }
