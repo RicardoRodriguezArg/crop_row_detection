e8f894960fa46c5954e967e889dcfaad36c94592
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2020-01-05 03:36:13 +0100

    add missing files

---
+++ map_compiler/photogrametric_pipeline/pipeline/keypoints_processor/include/keypoints_processor/feature_definitions.h
@@ -0,0 +1,9 @@
+#ifndef __FEATURE_DEFINITIONS_H__
+#define __FEATURE_DEFINITIONS_H__
+namespace FEATURE_CONSTANTS {
+
+    constexpr int MAX_SIZE_OF_THREAD = 5;
+    constexpr int MIN_SIZE_OF_THREAD = 1;
+    enum class KEYPROCESS_INFO { NOT_PROCESSED, OK, ERROR };
+} // namespace FEATURE_CONSTANTS
+#endif
